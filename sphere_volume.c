0beee8585cbbc67b454e47bc9dc6eb7c45ee8833
nikodimdev <nikodimkim@gmail.com>
2025-06-19 04:20:02 +0300

    Update sphere_volume.c

--- sphere_volume.c
+++ sphere_volume.c
@@ -11,29 +11,16 @@ main ()
   double volume;
   const double pi = 3.14159;
 
-
   printf ("Введите радиус сферы в метрах: ");
   scanf ("%lf", &radius);
-
 
   volume = NUM_PI * pi * pow(radius, 3);
 
   printf ("Объём сферы радиусом %.2f м: %.2f куб. м\n", radius, volume);
 
-
-
-    return 0;
+  return 0;
 }
 
 
 
 
-/*
-
-Задача из книги C Programming: A Modern Approach" (2nd Edition) by K.N. King
-Цель: написать программу которая выводит в резуотат вычисления объёма сферы
-
-
-
-
-*/
